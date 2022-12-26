15b19dc016a980f1c03957dde77bbc3157e185df
Doubleneck <antti.vuorenmaa@kolumbus.fi>
2022-12-26 20:07:06 +0200

    add preliminary functionality for news

--- hilland_front/src/App.js
+++ hilland_front/src/App.js
@@ -1,17 +1,89 @@
+import { useState , useEffect } from 'react'
+import axios from 'axios'
+
+const News = ({ news }) => {
+  // Base64 string data
+const data = news.picture
+const Example = ({ data }) => <img src={`data:image/jpeg;base64,${data}`} />
+
+//ReactDOM.render(<Example data={data} />, document.getElementById('container'))
+  return (
+    <ul >
+      <li ><h3>{news.title}</h3></li>
+      <li><Example data={data} /></li>
+      <li>{news.content}</li>
+    </ul>
+  )
+}
 
 const App = (props) => {
-  const { news } = props
-  return(
-  <div>
-    <h1>Hilland Demo</h1>
-    <h2>News</h2>
-    <ul>
-        <li>{news[0].content}</li>
-        <li>{news[1].content}</li>
-        <li>{news[2].content}</li>
+  const [news, setNews] = useState([])
+  const [newTitle, setNewTitle] = useState('')
+  const [newContent, setNewContent] = useState('')
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/news')
+      .then(response => {
+        console.log('promise fulfilled')
+        setNews(response.data)
+      })
+  }, [])
+
+  const handleTitleChange = (event) => {
+    setNewTitle(event.target.value)
+  }
+
+  const handleContentChange = (event) => {
+    setNewContent(event.target.value)
+  }
+
+ 
+  const addNews = (event) => {
+    event.preventDefault()
+    const current = new Date()
+    const date = `${current.getDate()}/${current.getMonth()+1}/${current.getFullYear()}`
+    const newsObject = {
+      title: newTitle,
+      content: newContent,
+      date: date,
+      picture: ""
+    }
+    axios
+      .post('http://localhost:3001/news', newsObject)
+      .then(response => {
+        setNews(news.concat(response.data))
+        setNewTitle('')
+        setNewContent('')
+        console.log(response)
+      })
+    console.log('button clicked', newsObject)
+  }
+
+  return (
+    <div>
+      <h1>Hilland Demo</h1>
+      <h2>News</h2>
+      <ul>
+        {news.map(news=> 
+          <News key={news.id} news={news} />
+        )}
       </ul>
-    
-  </div>
+      < NewsForm addNews={addNews} newTitle = {newTitle} newContent = {newContent} handleTitleChange={handleTitleChange}
+      handleContentChange = {handleContentChange}/>
+    </div>
+  )
+}
+
+const NewsForm = (props) => {
+  return(
+    <div>
+     <form onSubmit={props.addNews}>
+        <div> title: <input value={props.newTitle} onChange={props.handleTitleChange}/></div>
+        <div> content: <input value={props.newContent} onChange={props.handleContentChange}/></div> 
+        <button type="submit">add</button>
+      </form>
+    </div>
   )
 }
 
--- hilland_front/src/index.js
+++ hilland_front/src/index.js
@@ -2,36 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+//import axios from 'axios'
 import reportWebVitals from './reportWebVitals';
 
-
-const news = [
-  {
-    id: 1,
-    date: '2019-05-30T17:30:31.098Z',
-    title: 'News #1',
-    content: 'some news content #1',
-    picture: ''
-  },
-  {
-    id: 2,
-    date: '2019-05-30T17:30:31.098Z',
-    title: 'News #2',
-    content: 'some news content #3',
-    picture: ''
-  },
-  {
-    id: 3,
-    date: '2019-05-30T17:30:31.098Z',
-    title: 'News #3',
-    content: 'some news content #3',
-    picture: ''
-  },
-]
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <App news={news} />
-)
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+//axios.get('http://localhost:3001/news').then(response => {
+//  const news = response.data
+//  ReactDOM.createRoot(document.getElementById('root')).render(<App news={news} />)
+//})
 
 
 // If you want to start measuring performance in your app, pass a function
