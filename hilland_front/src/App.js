b0f1d7062f49ab27f7ea9c51f7cd86bd36177cdf
Doubleneck <antti.vuorenmaa@kolumbus.fi>
2022-12-26 15:58:36 +0200

    preliminary demos news

--- hilland_front/src/App.js
+++ hilland_front/src/App.js
@@ -1,7 +1,18 @@
-const App = () => (
+
+const App = (props) => {
+  const { news } = props
+  return(
   <div>
-    <p>Hilland Demo</p>
+    <h1>Hilland Demo</h1>
+    <h2>News</h2>
+    <ul>
+        <li>{news[0].content}</li>
+        <li>{news[1].content}</li>
+        <li>{news[2].content}</li>
+      </ul>
+    
   </div>
-)
+  )
+}
 
 export default App
--- hilland_front/src/index.js
+++ hilland_front/src/index.js
@@ -4,12 +4,35 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+const news = [
+  {
+    id: 1,
+    date: '2019-05-30T17:30:31.098Z',
+    title: 'News #1',
+    content: 'some news content #1',
+    picture: ''
+  },
+  {
+    id: 2,
+    date: '2019-05-30T17:30:31.098Z',
+    title: 'News #2',
+    content: 'some news content #3',
+    picture: ''
+  },
+  {
+    id: 3,
+    date: '2019-05-30T17:30:31.098Z',
+    title: 'News #3',
+    content: 'some news content #3',
+    picture: ''
+  },
+]
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <App news={news} />
+)
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
