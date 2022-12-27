e06ff1dadd41091ac654cdde66317a6e3b4cd4c4
Doubleneck <antti.vuorenmaa@kolumbus.fi>
2022-12-27 18:42:28 +0200

    working base64 image upload functionality

--- hilland_front/src/App.js
+++ hilland_front/src/App.js
@@ -1,11 +1,9 @@
 import { useState , useEffect } from 'react'
 import axios from 'axios'
-//import base64 from 'react-native-base64'
-import encode  from 'base-64'
 
 const News = ({ news }) => {
   const data = news.picture 
-    const Example = ({ data }) => <img src={`data:jpeg;base64,${data}`} />
+  const Example = ({ data }) => <img src={`data:jpeg;base64,${data}`} />
   
   return (
     <ul >
@@ -23,8 +21,6 @@ const App = (props) => {
   const [news, setNews] = useState([])
   const [newTitle, setNewTitle] = useState('')
   const [newContent, setNewContent] = useState('')
-  const [newFile, setNewFile] = useState(null);
-  const [newFilename, setNewFilename] = useState('Choose File');
   const [base64, setBase64] = useState("");
 
   useEffect(() => {
@@ -38,8 +34,6 @@ const App = (props) => {
   }, [])
 
   const handleTitleChange = (event) => {
-    console.log("newfile", newFile)
-    console.log("newfilename",newFilename)
     setNewTitle(event.target.value)
   }
 
@@ -47,60 +41,39 @@ const App = (props) => {
     setNewContent(event.target.value)
   }
 
-  const handlePhotoSelect = (e) => {
-    setNewFile(e.target.files[0])
-    setNewFilename(e.target.files[0].name)
+
+  const convertBase64 = (file) => {
+    return new Promise((resolve, reject) => {
+      const fileReader = new FileReader();
+      fileReader.readAsDataURL(file)
+      fileReader.onload = () => {
+        resolve(fileReader.result.split(',')[1]);
+      }
+      fileReader.onerror = (error) => {
+        reject(error);
+      }
+    })
   }
 
-  
+  const handlePhotoSelect = async (e) => {
+    const file = e.target.files[0]
+    let base64 = await convertBase64(file)
+    console.log(base64)
+    setBase64(base64)
+  }
+
   const addNews = (event) => {
     event.preventDefault()
     const current = new Date()
     const date = `${current.getDate()}/${current.getMonth()+1}/${current.getFullYear()}`
     console.log("addNews")
-
-    function previewFile() {
-      const file = newFile
-      //console.log("file",file)
-      const reader = new FileReader();
-    
-      reader.addEventListener("load", async () => {
-       function toBinary(string) {
-        const codeUnits = Uint16Array.from(
-          { length: string.length },
-          (element, index) => string.charCodeAt(index)
-        );
-        const charCodes = new Uint8Array(codeUnits.buffer);
-      
-        let result = "";
-        charCodes.forEach((char) => {
-          result += String.fromCharCode(char);
-        });
-        return result;
-      }
-       //setBase64(btoa(unescape(encodeURIComponent(reader.result))))
-       const converted = await toBinary(reader.result)
-       //console.log("converted to bin:", converted)
-       const my = await btoa(unescape(converted))
-       //console.log(my)
-       //console.log("converted to btoa:", my)
-       await setBase64(my)
-       //console.log(base64)
-       //const encodedstring = Buffer.from(reader.result).toString('base64')
-   
-      }, false);
-      if (file) {
-        console.log("onfilee")
-        reader.readAsText(file);
-      }
-    }
-    previewFile() 
+    console.log(base64)
     
     const newsObject = {
       title: newTitle,
       content: newContent,
       date: date,
-      picture: base64 //newFile//newFile//pictAsBase64
+      picture: base64 
     }
   
     const getData = async () => {
@@ -109,26 +82,15 @@ const App = (props) => {
       setNews(news.concat(response.data))
       setNewTitle('')
       setNewContent('')
-      setNewFile(null)
-      setNewFilename('Choose File')
+      setBase64('')
       console.log("Axios async",response.data)
       }
       catch (e) {
         console.log("problem")
-
       }
     };
     getData()
-  /*   axios
-      .post('http://localhost:3001/news', newsObject)
-      .then(response => {
-        setNews(news.concat(response.data))
-        setNewTitle('')
-        setNewContent('')
-        setNewFile(null)
-        setNewFilename('Choose File')
-        console.log(response)
-      }) */
+
     console.log('button clicked', newsObject)
   }
 
@@ -160,7 +122,5 @@ const NewsForm = (props) => {
     </div>
   )
 }
-
-
 
 export default App
