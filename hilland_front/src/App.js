c5b36b605874c4c5b1ce51b25510524d05b537bd
Doubleneck <antti.vuorenmaa@kolumbus.fi>
2023-01-04 18:56:25 +0200

    update working hours, add front login form functionality

--- hilland_front/src/App.js
+++ hilland_front/src/App.js
@@ -1,12 +1,16 @@
 import { useState , useEffect } from 'react'
 import newsService from './services/news'
+import loginService from './services/login'
 
 const App = (props) => {
   const [news, setNews] = useState([])
   const [newTitle, setNewTitle] = useState('')
   const [newContent, setNewContent] = useState('')
   const [newURL, setNewURL] = useState('')
-  const [base64, setBase64] = useState("");
+  const [base64, setBase64] = useState("")
+  const [username, setUsername] = useState('') 
+  const [password, setPassword] = useState('') 
+  const [user, setUser] = useState('')
 
   useEffect(() => {
     newsService
@@ -15,6 +19,26 @@ const App = (props) => {
         setNews(response.data)
       })
   }, [])
+  
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    console.log('logging in with', username, password)
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+      console.log('user', user)
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      console.log('kukkuu')
+      alert('wrong credentials')
+      /* setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000) */
+    }
+  }
 
   const handleTitleChange = (event) => {
     setNewTitle(event.target.value)
@@ -102,6 +126,28 @@ const App = (props) => {
   return (
     <div>
       <h1>Hilland Demo</h1>
+      <h2>Login</h2>
+      <form onSubmit={handleLogin}>
+        <div>
+          username
+            <input
+            type="text"
+            value={username}
+            name="Username"
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          password
+            <input
+            type="password"
+            value={password}
+            name="Password"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type="submit">login</button>
+      </form>
       <h2>News</h2>
       <ul>
         {news.map(news=> 
