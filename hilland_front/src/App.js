307ba5292e88c226a971193572d47104debae77e
Doubleneck <antti.vuorenmaa@kolumbus.fi>
2023-01-05 19:00:05 +0200

    add front local storage token, add sorting for news, fix bug in create

--- hilland_front/src/App.js
+++ hilland_front/src/App.js
@@ -15,9 +15,16 @@ const App = (props) => {
   useEffect(() => {
     newsService
       .getAll()
-      .then(response => {
-        setNews(response.data)
-      })
+      .then(news => setNews( news.sort((a, b) => b.date.localeCompare(a.date) )))
+  }, [news])
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedHillandappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      newsService.setToken(user.token)
+    }
   }, [])
   
   const handleLogin = async (event) => {
@@ -25,14 +32,18 @@ const App = (props) => {
     console.log('logging in with', username, password)
     try {
       const user = await loginService.login({
-        username, password,
+        username, password
       })
-      console.log('user', user)
+      window.localStorage.setItem(
+        'loggedHillandappUser', JSON.stringify(user)
+      ) 
+      //console.log('user', user.token)
+      newsService.setToken(user.token)
       setUser(user)
       setUsername('')
       setPassword('')
     } catch (exception) {
-      console.log('kukkuu')
+      
       alert('wrong credentials')
       /* setTimeout(() => {
         setErrorMessage(null)
@@ -92,9 +103,7 @@ const App = (props) => {
       })
     } 
   }
-
   const addNews = (event) => {
-    event.preventDefault()
     const current = new Date()
     const date = `${current.getDate()}/${current.getMonth()+1}/${current.getFullYear()}`
 
@@ -105,8 +114,38 @@ const App = (props) => {
       date: date,
       image: base64
     }
-  
     newsService
+      .create(newsObject)
+      .then(returnedNews=> {
+        setNews(news.concat(returnedNews))
+      })
+      alert(`A news: ${newsObject.title}  added !!!`)
+      .catch((error) => {
+        error('something went wrong while trying to add a news', error.message)
+      })
+  }
+/*   const addNews = async (event) => {
+    event.preventDefault()
+    const current = await new Date()
+    const date = `${current.getDate()}/${current.getMonth()+1}/${current.getFullYear()}`
+
+    const newsObject = await{
+      title: newTitle,
+      content: newContent,
+      url: newURL,
+      date: date,
+      image: base64
+    }
+    const response = await newsService.create(newsObject)
+    await alert ('created a news!')
+    //await news.concat(response.data)
+    await setNews(news.concat(response.data))
+    setNewTitle('')
+        setNewContent('')
+        setNewURL('')
+      } */
+
+  /*   newsService
       .create(newsObject)
       .then(response => {
         setNews(news.concat(response.data))
@@ -120,50 +159,93 @@ const App = (props) => {
         alert(
           `Can´t get news`
         )
-      })
+      }) */
+  
+  const News = ({ news, removeNews}) => {
+    const data = news.image
+    const Image = ({ data }) => <img src={`data:jpeg;base64,${data}`} alt = 'alt description'/>
+    
+    return (
+      <ul >
+        <li ><h3>{news.title}</h3></li>
+        <li> <Image data = {data} /> </li>
+        <li>{news.content}</li>
+        <li>URL:{news.url}</li>
+        <button value = {news.id} onClick={removeNews}>
+          delete 
+        </button>
+      </ul>
+    )
   }
+  const loginForm = () => (
+    <div>
+    <h2>Login</h2>
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+          <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+          <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>   
+    </div>   
+  )
 
+  const newsForm = () => {
+    return(
+      <div>
+       <h2>Add news: </h2>
+       <form onSubmit={addNews}>
+          <div> title: <input value={newTitle} onChange={handleTitleChange}/></div>
+          <div> content: <input value={newContent} onChange={handleContentChange}/></div> 
+          <div> url: <input value={newURL} onChange={handleURLChange}/></div> 
+          <br />
+          <div> file: <input type="file" onChange={handlePhotoSelect}/></div>
+          <button type="submit">add</button>
+        </form>
+      </div>
+    )
+  }
   return (
     <div>
+      
       <h1>Hilland Demo</h1>
-      <h2>Login</h2>
-      <form onSubmit={handleLogin}>
-        <div>
-          username
-            <input
-            type="text"
-            value={username}
-            name="Username"
-            onChange={({ target }) => setUsername(target.value)}
-          />
-        </div>
-        <div>
-          password
-            <input
-            type="password"
-            value={password}
-            name="Password"
-            onChange={({ target }) => setPassword(target.value)}
-          />
-        </div>
-        <button type="submit">login</button>
-      </form>
+      
+      {user === '' ?
+      loginForm() :
+      <div>
+        <p>{user.name} logged in</p>
+        {newsForm()}
+      </div>
+    }
+      
       <h2>News</h2>
       <ul>
         {news.map(news=> 
           <News key={news.id} news={news} removeNews = {removeNews} />
         )}
       </ul>
-      < NewsForm addNews={addNews} newURL = {newURL} newTitle = {newTitle} newContent = {newContent} 
-      handleURLChange = {handleURLChange} handleTitleChange={handleTitleChange}
-      handleContentChange = {handleContentChange} handlePhotoSelect ={handlePhotoSelect} />
+     
     </div>
   )
 }
 
-const News = ({ news, removeNews}) => {
+/* const News = ({ news, removeNews}) => {
   const data = news.image
-  const Image = ({ data }) => <img src={`data:jpeg;base64,${data}`} />
+  const Image = ({ data }) => <img src={`data:jpeg;base64,${data}`} alt = 'alt description'/>
   
   return (
     <ul >
@@ -176,10 +258,10 @@ const News = ({ news, removeNews}) => {
       </button>
     </ul>
   )
-}
+} */
 
 
-const NewsForm = (props) => {
+/* const NewsForm = (props) => {
   return(
     <div>
      <form onSubmit={props.addNews}>
@@ -192,6 +274,30 @@ const NewsForm = (props) => {
       </form>
     </div>
   )
-}
+} */
+
+/* const loginForm = () => (
+  <form onSubmit={handleLogin}>
+    <div>
+      username
+        <input
+        type="text"
+        value={username}
+        name="Username"
+        onChange={({ target }) => setUsername(target.value)}
+      />
+    </div>
+    <div>
+      password
+        <input
+        type="password"
+        value={password}
+        name="Password"
+        onChange={({ target }) => setPassword(target.value)}
+      />
+    </div>
+    <button type="submit">login</button>
+  </form>      
+) */
 
 export default App
--- hilland_front/src/services/news.js
+++ hilland_front/src/services/news.js
@@ -1,20 +1,40 @@
 import axios from 'axios'
 const baseUrl = 'http://localhost:3001/api/news'
+let token = null
 
-const getAll = () => {
-  return axios.get(baseUrl)
+const setToken = newToken => {
+  token = `bearer ${newToken}`
+}
+const getAll =  () => {
+  const request = axios.get(baseUrl)
+  return request.then(response => response.data)
 }
 
-const create = newObject => {
-  return axios.post(baseUrl, newObject)
+const create = async newObject => {
+  const config = {
+    headers: { Authorization: token },
+  }
+  const response = await axios.post(baseUrl, newObject, config)
+  return response.data
+  //return axios.post(baseUrl, newObject)
 }
 
 const update = (id, newObject) => {
-  return axios.put(`${baseUrl}/${id}`, newObject)
+  const config = {
+    headers: { Authorization: token },
+  }
+  const response =  axios.put(`${baseUrl}/${id}`, newObject, config)
+  return response.data
+  //return axios.put(`${baseUrl}/${id}`, newObject)
 }
 
-const remove = (id) => {
-    return axios.delete(`${baseUrl}/${id}`)
+const remove = async (id) => {
+  const config = {
+    headers: { Authorization: token },
+  }
+  const response = axios.delete(`${baseUrl}/${id}`, config)
+  return response.data
+   // return axios.delete(`${baseUrl}/${id}`)
   }
 
-export default { getAll, create, update, remove }
+export default { getAll, create, update, remove , setToken}
