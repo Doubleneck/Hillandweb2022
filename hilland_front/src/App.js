646a045470cf89079ed80cb29cf8af10bf448a3f
Doubleneck <antti.vuorenmaa@kolumbus.fi>
2023-01-10 02:07:12 +0200

    clean code

--- hilland_front/src/App.js
+++ hilland_front/src/App.js
@@ -1,108 +1,120 @@
-import { useState , useEffect } from 'react'
-import newsService from './services/news'
-import loginService from './services/login'
-import LoginForm from './components/LoginForm'
-import NewsForm from './components/NewsForm'
-import Togglable from './components/Togglable'
+import { useState, useEffect } from "react"
+import newsService from "./services/news"
+import loginService from "./services/login"
+import LoginForm from "./components/LoginForm"
+import NewsForm from "./components/NewsForm"
+import Togglable from "./components/Togglable"
 
 const App = () => {
   const [news, setNews] = useState([])
-  const [user, setUser] = useState('')
+  const [user, setUser] = useState("")
 
   useEffect(() => {
     newsService
       .getAll()
-      .then(news => setNews( news.sort((a, b) => b.date.localeCompare(a.date) )))
+      .then((news) =>
+        setNews(news.sort((a, b) => b.date.localeCompare(a.date)))
+      )
   }, [])
 
   useEffect(() => {
-    const loggedUserJSON = window.localStorage.getItem('loggedHillandappUser')
+    const loggedUserJSON = window.localStorage.getItem("loggedHillandappUser")
     if (loggedUserJSON) {
       const user = JSON.parse(loggedUserJSON)
       setUser(user)
       newsService.setToken(user.token)
     }
   }, [])
-  
+
   const handleLogin = async (userObject) => {
     try {
       const user = await loginService.login(userObject)
-      window.localStorage.setItem('loggedHillandappUser', JSON.stringify(user)) 
+      window.localStorage.setItem("loggedHillandappUser", JSON.stringify(user))
       newsService.setToken(user.token)
       setUser(user)
-      console.log('logging in with', userObject.username, userObject.password)
+      console.log("logging in with", userObject.username, userObject.password)
     } catch (exception) {
-      alert('wrong credentials')
+      alert("wrong credentials")
     }
   }
 
   const removeNewsObject = (event) => {
     event.preventDefault()
-    const thisnews =  news.filter(n => n.id.toString() === event.target.value.toString())[0]
+    const thisnews = news.filter(
+      (n) => n.id.toString() === event.target.value.toString()
+    )[0]
     if (window.confirm(`Delete ${thisnews.title}?`)) {
-      newsService
-        .remove(event.target.value)
-        .then(() => {
-          alert(
-            `Removed ${thisnews.title} from News `
-          )
-          setNews(news.filter(n => n.id.toString() !== event.target.value))
+      newsService.remove(event.target.value).then(() => {
+        alert(`Removed ${thisnews.title} from News `)
+        setNews(news.filter((n) => n.id.toString() !== event.target.value))
       })
-    } 
+    }
   }
-  
-  const addNews = (newsObject) => {
-    newsService
-      .create(newsObject)
-      .then(returnedNews=> {
-        setNews(news.concat(returnedNews))
-      })
+
+  const addNews = async (newsObject) => {
+    //console.log('addNewssissä',newsObject.imageFile.size)
+    try {
+      const newsDataObject = {
+        title: newsObject.title, 
+        content: newsObject.content,
+        url: newsObject.URL,
+        date: newsObject.date,
+        imageURL: ""
+      }
+    
+      const returnedNews = await newsService.create(newsDataObject)
+      setNews(news.concat(returnedNews).sort((a, b) => b.date.localeCompare(a.date)))
       alert(`A news: ${newsObject.title}  added !!!`)
-      .catch((error) => {
-        error('something went wrong while trying to add a news', error.message)
-      })
+    } catch (exception) {
+      alert("something went wrong while trying to create news")
+    }
   }
-  
-  const NewsObject = ({ newsObject, removeNewsObject, updateNews}) => {
-    //const data = newsObject.image
-    //const Image = ({ data }) => <img src={`data:jpeg;base64,${data}`} alt = 'newsphoto'/>
+
+  const NewsObject = ({ newsObject, removeNewsObject, updateNews }) => {
     return (
       <ul>
-        <li ><h3>{newsObject.title}</h3></li>
-        <li> <img src={newsObject.imageURL}/> </li>
-       
+        <li>
+          <h3>{newsObject.title}</h3>
+        </li>
+        <li>
+          {" "}
+          <img src={newsObject.imageURL} />{" "}
+        </li>
+
         <li>{newsObject.content}</li>
         <li>URL:{newsObject.url}</li>
-        <button value = {newsObject.id} onClick={removeNewsObject}>
-          delete 
+        <button value={newsObject.id} onClick={removeNewsObject}>
+          delete
         </button>
       </ul>
     )
   }
-  
+
   return (
     <div>
       <h1>Hilland Demo</h1>
-      <Togglable buttonLabel="Static pict">
-      <img src="http://localhost:3001/testpict.jpg" alt = "testikuva"/>
-      </Togglable>
-      
-      {user === '' ?
-      <LoginForm handleSubmit={handleLogin} /> :
-      <div>
-      <p>{user.name} logged in</p>
-      <Togglable buttonLabel="Add News">
-        <NewsForm createNews={addNews}/>
-      </Togglable>
-      </div>
-    }
+      {user === "" ? (
+        <LoginForm handleSubmit={handleLogin} />
+      ) : (
+        <div>
+          <p>{user.name} logged in</p>
+          <Togglable buttonLabel='Add News'>
+            <NewsForm createNews={addNews} />
+          </Togglable>
+        </div>
+      )}
       <h2>News</h2>
       <ul>
-        {news.map(newsObject=> 
-          <NewsObject key={newsObject.id} newsObject={newsObject} removeNewsObject = {removeNewsObject} />
-        )}
+        {news.map((newsObject) => (
+          <NewsObject
+            key={newsObject.id}
+            newsObject={newsObject}
+            removeNewsObject={removeNewsObject}
+          />
+        ))}
       </ul>
     </div>
-})
+  )
+}
 
 export default App
--- hilland_front/src/components/NewsForm.js
+++ hilland_front/src/components/NewsForm.js
@@ -1,76 +1,83 @@
-import { useState } from 'react' 
+import { useState } from "react"
 
 const NewsForm = ({ createNews }) => {
-    const current = new Date()
-    const date = `${current.getDate()}/${current.getMonth()+1}/${current.getFullYear()}`
-    const [newTitle, setNewTitle] = useState('')
-    const [newContent, setNewContent] = useState('')
-    const [newURL, setNewURL] = useState('')
-    const [imageURL, setImageURL] = useState("")
+  const current = new Date()
+  const date = `${current.getDate()}/${
+    current.getMonth() + 1
+  }/${current.getFullYear()}`
+  const [newTitle, setNewTitle] = useState("")
+  const [newContent, setNewContent] = useState("")
+  const [newURL, setNewURL] = useState("")
+  const [imageURL, setImageURL] = useState("")
+  const [imageFile, setImageFile] = useState('')
 
-    const handleTitleChange = (event) => {
-        setNewTitle(event.target.value)
-      }
-    
-      const handleContentChange = (event) => {
-        setNewContent(event.target.value)
-      }
-    
-      const handleURLChange = (event) => {
-        setNewURL(event.target.value)
-      }
-    
-    /*   const convertBase64 = (file) => {
-        return new Promise((resolve, reject) => {
-          const fileReader = new FileReader();
-          fileReader.readAsDataURL(file)
-          fileReader.onload = () => {
-            resolve(fileReader.result.split(',')[1]);
-          }
-          fileReader.onerror = (error) => {
-            reject(error);
-          }
-        })
-      } */
-    
-      const handlePhotoSelect = async (e) => {
-        const file = e.target.files[0]
-        const MAX_FILE_SIZE = 1000 // 300kB
-        if (file.name.split('.')[1]==="jpg" && file.size/1000 < MAX_FILE_SIZE ){
-          //let base64 = await convertBase64(file)
-          //console.log(base64)
-          //setBase64(base64)
-        } else {
-          alert("Kuvan maksimikoko on 1M ja sen pitää olla jpg")
-        }
-      }
-    const newsObject = {
-      title: newTitle,
-      content: newContent,
-      url: newURL,
-      date: date,
-      imageURL: imageURL
+  const handleTitleChange = (event) => {
+    setNewTitle(event.target.value)
+  }
+
+  const handleContentChange = (event) => {
+    setNewContent(event.target.value)
+  }
+
+  const handleURLChange = (event) => {
+    setNewURL(event.target.value)
+  }
+
+
+  const handlePhotoSelect = async (e) => {
+    e.preventDefault()
+    const file = e.target.files[0]
+    const MAX_FILE_SIZE = 1000 
+    if ( file.size / 1000 < MAX_FILE_SIZE) {
+      setImageFile(file)
+    } else {
+      alert("Kuvan maksimikoko on 1M")
     }
-    const addNews = (event) => {
-        event.preventDefault()
-        createNews(newsObject)
-        setNewTitle('')
-        setNewContent('')
-        setNewURL('')
-        setImageURL('')
-    }
-return(
+  }
+  const newsObject = {
+    title: newTitle, 
+    content: newContent,
+    url: newURL,
+    date: date,
+    imageURL: imageURL,
+    imageFile: imageFile
+  }
+  const addNews = (event) => {
+    event.preventDefault()
+    createNews(newsObject)
+    setNewTitle("")
+    setNewContent("")
+    setNewURL("")
+    setImageURL("")
+    setImageFile("")
+    
+  }
+  return (
     <div>
-       <h2>Add news: </h2>
-       <form onSubmit={addNews}>
-          <div> title: <input value={newTitle} onChange={handleTitleChange}/></div>
-          <div> content: <input value={newContent} onChange={handleContentChange}/></div> 
-          <div> url: <input value={newURL} onChange={handleURLChange}/></div> 
-          <br />
-          <div> file: <input type="file" onChange={handlePhotoSelect}/></div>
-          <button type="submit">add</button>
-        </form>
-      </div>
-)}
+      <h2>Add news: </h2>
+      <form onSubmit={addNews}>
+        <div>
+          title: <input value={newTitle} onChange={handleTitleChange} />
+        </div>
+        <div>
+          content: <input value={newContent} onChange={handleContentChange} />
+        </div>
+        <div>
+          url: <input value={newURL} onChange={handleURLChange} />
+        </div>
+        <br />
+        <div>
+          file:
+          <input
+            type='file'
+            accept='image/*'
+            onChange={handlePhotoSelect}
+          />
+        </div>
+        <button type='submit'>Upload</button>
+      </form>
+    </div>
+  )
+}
 
 export default NewsForm
