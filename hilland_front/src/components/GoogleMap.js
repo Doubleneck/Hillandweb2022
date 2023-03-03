d28e2587d5212f0c49413e2d4f6e7aba8311026f
Doubleneck <antti.vuorenmaa@kolumbus.fi>
2023-03-03 21:30:47 +0200

    Add Google Maps & FB Link to Home Page front

---
+++ hilland_front/src/components/GoogleMap.js
@@ -0,0 +1,9 @@
+const GoogleMap=()=>{
+    return (
+        <div className="google-map-code">
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d992.0571602497945!2d24.946202887001764!3d60.178839989125215!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46920bd360b9d6b9%3A0xe6852ffa3777065a!2sPaasitorni!5e0!3m2!1sfi!2sfi!4v1677870861666!5m2!1sfi!2sfi" 
+          title='paasitorni' width="100%" height="450" style={{border:0}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+    );
+ }
+ export default  GoogleMap
