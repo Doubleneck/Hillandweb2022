a322a98217110122c8ddbf35f684f541f750d4d2
Doubleneck <antti.vuorenmaa@kolumbus.fi>
2023-03-04 14:59:15 +0200

    refactor notificationReducer hilland_front

--- hilland_front/src/reducers/notificationReducer.js
+++ hilland_front/src/reducers/notificationReducer.js
@@ -7,14 +7,9 @@ const notificationSlice = createSlice({
   initialState,
   reducers: {
     createNotification(state, action) {
-      if (state.timeout) {
-        console.log('timeout löytyy:', state.timeout)
-        clearTimeout(state.timeout)
-      }
       console.log('action.payload.content:', action.payload.content)
       state.content = action.payload.content
       state.type = action.payload.type
-      state.timeout = action.payload.timeoutID
       return state
     },
     clearNotification() {
@@ -28,15 +23,14 @@ export const setNotification = (content, time, type) => {
   console.log('setNotification ', content, time)
   const ms = time * 1000
   return async (dispatch) => {
-    const timeoutID = setTimeout(() => {
-      dispatch(clearNotification())
-    }, ms)
     const notification = {
       content: content,
-      timeoutID: timeoutID,
       type: type,
     }
     dispatch(createNotification(notification))
+    setTimeout(() => {
+      dispatch(clearNotification())
+    }, ms)
   }
 }
 
